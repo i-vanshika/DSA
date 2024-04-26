a7238ca70444aeb0489d34d8cbb352ad5a2eca2a
Vanshika Verma <vanshika.j.verma@gmail.com>
2024-04-26 07:24:05 +0530

    Basics

--- Linked_list/1D_linked_list/insertion_deletion.cpp
+++ Linked_list/1D_linked_list/insertion_deletion.cpp
@@ -61,10 +61,37 @@ Node* removesTail(Node* head){
 
 }
 
+/* DELETE Kth ELEMENT FROM A LL */
+Node* removeK(Node* head, int k){
+    if(head == NULL ) return head;
+    if(k==1){
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    int count = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL){
+        count ++;
+        if(count == k){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
 int main(){
     vector<int>arr = {12,5,8,7};
     Node* head = convertArr2LL(arr);
     //head = removesHead(head); 
-    head = removesTail(head);
+    //head = removesTail(head);
+    head = removeK(head, 3);
     print(head);
 }
